deb04b71ce7789f2f320ceeac97e5662c83e5b08
Taylor Downs <downs.taylor@gmail.com>
2016-06-15 16:48:26 +0200

    upgrade to newer helper functions

--- job-library/upsert-many-items-in-array-with-value-outside-array.js
+++ job-library/upsert-many-items-in-array-with-value-outside-array.js
@@ -1,13 +1,11 @@
-steps(
    each(
       merge(dataPath("form.ID_cards_given_to_vendor[*]"), fields(
         field("Vendor_Id", dataValue("form.ID_vendor")),
         field("form_finished_time", dataValue("form.meta.timeEnd"))
       )),
       upsert("Small_Packet__c", "sp_id__c", fields(
-         field("sp_id__c", sourceValue("$.data.ID_cards_given_to_vendor")),
-         lookup("Vendor__r", "Badge_Code__c", "$.data.Vendor_Id"),
-         field("Small_Packet_Distribution_Date__c", sourceValue("$.data.form_finished_time"))
+         field("sp_id__c", dataValue("ID_cards_given_to_vendor")),
+         relationship("Vendor__r", "Badge_Code__c", dataValue("Vendor_Id")),
+         field("Small_Packet_Distribution_Date__c", dataValue("form_finished_time"))
       ))
    )
-)
