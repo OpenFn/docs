c298552891772aa7d002a03d03e2f6894bf37294
Taylor Downs <downs.taylor@gmail.com>
2016-06-15 16:46:04 +0200

    merged timeEnd to be used in child array

--- job-library/upsert-many-items-in-array-with-value-outside-array.js
+++ job-library/upsert-many-items-in-array-with-value-outside-array.js
@@ -1,9 +1,13 @@
 steps(
    each(
-     join("$.data.form.ID_cards_given_to_vendor[*]", "$.data.form.ID_vendor", "Vendor_Id"),
-     upsert("Small_Packet__c", "sp_id__c", fields(
-       field("sp_id__c", sourceValue("$.data.ID_cards_given_to_vendor")),
-       lookup("Vendor__r", "Badge_Code__c", "$.data.Vendor_Id")
-     ))
+      merge(dataPath("form.ID_cards_given_to_vendor[*]"), fields(
+        field("Vendor_Id", dataValue("form.ID_vendor")),
+        field("form_finished_time", dataValue("form.meta.timeEnd"))
+      )),
+      upsert("Small_Packet__c", "sp_id__c", fields(
+         field("sp_id__c", sourceValue("$.data.ID_cards_given_to_vendor")),
+         lookup("Vendor__r", "Badge_Code__c", "$.data.Vendor_Id"),
+         field("Small_Packet_Distribution_Date__c", sourceValue("$.data.form_finished_time"))
+      ))
    )
- )
+)
