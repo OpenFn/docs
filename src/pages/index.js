23b416f49a4ee27164090eb7cf01edc1271241ba
Taylor Downs <downs.taylor@gmail.com>
2021-01-21 13:40:59 +0000

    add links to features

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,13 +43,21 @@ function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <Link
+        style={{
+          color: 'var(--ifm-font-color-base)',
+          textDecoration: 'none',
+        }}
+        to={useBaseUrl(title.toLowerCase())}
+      >
+        {imgUrl && (
+          <div className="text--center">
+            <img className={styles.featureImage} src={imgUrl} alt={title} />
+          </div>
+        )}
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </Link>
     </div>
   );
 }
