422e27585f43757921dad9a510b067ed8791c5bf
gh-actions <actions@gihub.com>
2021-12-09 13:46:01 +0000

    Deploy website - based on f4ab13c9688a4d473874b3c263f4d100dea80b71

--- fr/assets/js/c059c561.b0b7137d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_openfn_docs=self.webpackChunk_openfn_docs||[]).push([[3754],{14258:function(s){s.exports=JSON.parse('{"allTagsPath":"/fr/articles/tags","slug":"/fr/articles/tags/conseils","name":"conseils","count":7,"permalink":"/fr/articles/tags/conseils"}')}}]);
---
+++ fr/assets/js/ead2dbe0.ffbd06a0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_openfn_docs=self.webpackChunk_openfn_docs||[]).push([[944],{99900:function(e){e.exports=JSON.parse('{"allTagsPath":"/fr/articles/tags","slug":"/fr/articles/tags/annoucement","name":"annoucement","count":1,"permalink":"/fr/articles/tags/annoucement"}')}}]);
