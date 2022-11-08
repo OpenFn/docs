d9fe2ca257fffb4d7cac9f3cda12436d8a8032ba
Taylor Downs <taylor@openfn.org>
2022-11-08 11:16:05 +0000

    particles fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import Particles from 'react-tsparticles';
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
 import styles from './styles.module.css';
 
 const features = [
@@ -92,10 +93,135 @@ function Feature({ imageUrl, title, description, link }) {
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
+
+  const particlesInit = useCallback(async engine => {
+    await loadFull(engine);
+  }, []);
+
+  // const particlesLoaded = useCallback(async container => {
+  //   await console.log(container);
+  // }, []);
+
   return (
     <Layout title={`Home`} description="The OpenFn Documentation Site">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <Particles style={{ position: 'absolute', top: 0 }} />
+        {/* <Particles id="tsparticles" style={{ position: 'absolute', top: 0 }} /> */}
+        <Particles
+          style={{ position: 'absolute', top: 0 }}
+          id="tsparticles"
+          init={particlesInit}
+          // loaded={particlesLoaded}
+          options={{
+            particles: {
+              number: {
+                value: 60,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              color: {
+                value: '#ffffff',
+              },
+              shape: {
+                type: 'circle',
+                stroke: {
+                  width: 0,
+                  color: '#000000',
+                },
+                polygon: {
+                  nb_sides: 5,
+                },
+                image: {
+                  src: 'img/github.svg',
+                  width: 100,
+                  height: 100,
+                },
+              },
+              opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                  enable: false,
+                  speed: 1,
+                  opacity_min: 0.1,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: '#ffffff',
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 1.5,
+                direction: 'none',
+                random: false,
+                straight: false,
+                out_mode: 'out',
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200,
+                },
+              },
+            },
+            interactivity: {
+              detect_on: 'canvas',
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'grab',
+                },
+                onclick: {
+                  enable: true,
+                  mode: 'push',
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 200,
+                  line_linked: {
+                    opacity: 1,
+                  },
+                },
+                bubble: {
+                  distance: 400,
+                  size: 40,
+                  duration: 2,
+                  opacity: 8,
+                  speed: 3,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
         <div className="container" style={{ zIndex: 1 }}>
           <h1 className="hero__title">OpenFn Documentation</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
