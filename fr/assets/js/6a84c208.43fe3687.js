9b02287fd417ed180cec9e759ee4800cdf20a927
gh-actions <actions@gihub.com>
2023-05-31 20:21:27 +0000

    Deploy website - based on d3f23a09f785ec0e96e38125d786961ce5f485c8

---
+++ fr/assets/js/6a84c208.43fe3687.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_openfn_docs=self.webpackChunk_openfn_docs||[]).push([[74999],{36730:e=>{e.exports=JSON.parse('{"permalink":"/fr/blog/page/2","page":2,"postsPerPage":10,"totalPages":3,"totalCount":21,"previousPage":"/fr/blog","nextPage":"/fr/blog/page/3","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
