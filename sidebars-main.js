53f6c2d5f49a328478080cf35e5c5d3a2168cfc9
Emmanuel Evance <mtuchidev@gmail.com>
2023-12-13 11:57:54 +0300

    add cli installation on sidebar menu

--- sidebars-main.js
+++ sidebars-main.js
@@ -39,6 +39,7 @@ module.exports = {
       label: 'Build (For Developers)',
       items: [
         'build-for-developers/cli-intro',
+        'build-for-developers/cli-installation',
         'build-for-developers/cli-usage',
         'build-for-developers/cli-tutorial',
         'build-for-developers/build-with-api',
