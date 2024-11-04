857224fc290e979006afd0e997048df624dccef6
Hunter Achieng <achienghunter@gmail.com>
2024-11-04 18:10:39 +0300

    remove submitXls
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- adaptors/library/jobs/commcare-asri-submitxls.js
+++
@@ -1,8 +0,0 @@
-// Update the kunjungan case with a new row of data. The row contains the case_id and satusehat_id
-
-submitXls([{ case_id: 'abcde', satusehat_id: '12345' }], {
-    case_type: 'kunjungan',
-    search_field: 'case_id',
-    search_column: 'case_id',
-    create_new_cases: 'off',
-  });
