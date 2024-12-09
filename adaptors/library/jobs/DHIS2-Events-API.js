23466eee405058e9ce7025e2c4deb2d2b7618418
Emmanuel Evance <mtuchidev@gmail.com>
2024-12-09 21:49:34 +0300

    update dhis2 examples

--- adaptors/library/jobs/DHIS2-Events-API.js
+++ adaptors/library/jobs/DHIS2-Events-API.js
@@ -1,25 +1,38 @@
 // ----
 // Create new events in DHIS2 using a generic JSON message, submitted by
-// Taylor Downs @ OpenFn for demonstration porpoises.
+// Taylor Downs @ OpenFn, Co-authored by @mtuchi
 // ---
 
-event(
-  fields(
-    field('program', 'eBAyeGv0exc'),
-    field('orgUnit', 'DiszpKrYNg8'),
-    field('eventDate', dataValue('meta.date')),
-    field('status', 'COMPLETED'),
-    field('storedBy', 'admin'),
-    field('coordinate', {
-      latitude: '59.8',
-      longitude: '10.9',
-    }),
-    field('dataValues', function (state) {
-      return [
-        dataElement('qrur9Dvnyt5', dataValue('form.prop_a')(state)),
-        dataElement('oZg33kd9taw', dataValue('form.prop_b')(state)),
-        dataElement('msodh3rEMJa', dataValue('form.prop_c')(state)),
-      ];
-    })
-  )
-);
+fn(state => {
+  const { meta, form } = state.data;
+
+  state.events = {
+    program: 'eBAyeGv0exc',
+    orgUnit: 'DiszpKrYNg8',
+    occurredAt: meta.date,
+    status: 'COMPLETED',
+    storedBy: 'admin',
+    geometry: {
+      type: 'POINT',
+      coordinates: [59.8, 10.9],
+    },
+    dataValues: [
+      {
+        dataElement: 'qrur9Dvnyt5',
+        value: form.prop_a,
+      },
+      {
+        dataElement: 'oZg33kd9taw',
+        value: form.prop_b,
+      },
+      {
+        dataElement: 'msodh3rEMJa',
+        value: form.prop_c,
+      },
+    ],
+  };
+
+  return state;
+});
+
+create('events', state => state.events);
