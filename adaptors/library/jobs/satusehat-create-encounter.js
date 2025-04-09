72ad04ceb16a1f120e35c757df736b5300842d11
Hunter Achieng <achienghunter@gmail.com>
2025-04-09 14:01:54 +0300

    feat: Add create encounter and observations examples for satusehat
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

---
+++ adaptors/library/jobs/satusehat-create-encounter.js
@@ -0,0 +1,105 @@
+// Create an encounter in Satusehat
+
+fn(state => {
+  state.organizationId = state.visit.organizationId;
+  state.encounterId = util.uuid();
+
+  state.encounterMapping = {
+
+
+      id: state.encounterId,
+      resourceType: 'Encounter',
+      identifier: [
+        {
+          system: `http://sys-ids.kemkes.go.id/encounter/${state.organizationId}`,
+          value: state.visit.case_id,
+        },
+      ],
+      status: 'arrived',
+      class: {
+        system: 'http://terminology.hl7.org/CodeSystem/v3-ActCode',
+        code: 'AMB',
+        display: 'ambulatory',
+      },
+      subject: {
+        reference: 'Patient/P00805884304',
+
+        display: 'Elizabeth Dior',
+      },
+      participant: [
+        {
+          type: [
+            {
+              coding: [
+                {
+                  system:
+                    'http://terminology.hl7.org/CodeSystem/v3-ParticipationType',
+                  code: 'ATND',
+                  display: 'attender',
+                },
+              ],
+            },
+          ],
+          individual: {
+            reference: `Practitioner/N10000001`,
+            display: 'Voigt',
+          },
+        },
+      ],
+      period: {
+        start: state.visit.visit_date,
+        end: state.visit.visit_date,
+      },
+
+      location: [
+        {
+          location: {
+            reference: `Location/574d5ffd-2dc2-453b-a787-d1c63cc89ae4`,
+            display: `PLKB`,
+          },
+          extension: [
+            {
+              url: 'https://fhir.kemkes.go.id/r4/StructureDefinition/ServiceClass',
+              extension: [
+                {
+                  url: 'value',
+                  valueCodeableConcept: {
+                    coding: [
+                      {
+                        system:
+                          'http://terminology.kemkes.go.id/CodeSystem/locationServiceClass-Outpatient',
+                        code: 'reguler',
+                        display: 'Kelas Reguler',
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      statusHistory: [
+        {
+          status: 'arrived',
+          period: {
+            start: state.visit.visit_date,
+            end: state.visit.visit_date,
+          },
+        },
+      ],
+      serviceProvider: {
+        reference: `Organization/${state.organizationId}`,
+      },
+    
+  };
+
+  return state;
+});
+
+post('Encounter', $.encounterMapping);
+
+fn(state => {
+  state.encounterData = state.data;
+  return state;
+});
---
+++ adaptors/library/jobs/satusehat-create-observations.js
@@ -0,0 +1,144 @@
+// Create an observation in Satusehat
+
+fn(state => {
+  function createObservationData(item, state, result) {
+    const patientReference = `Patient/${
+      state.patient.ihs_number ?? state.patient.properties.ihs_number
+    }`;
+    const encounterReference = `Encounter/${state.encounterId}`;
+    const encounterDisplay = `${state.patient.full_name}'s ${state.visit.properties.visit_type} on ${state.visit.properties.visit_date}`;
+    return {
+      resourceType: 'Observation',
+      status: 'final',
+      category: [
+        {
+          coding: item.category,
+        },
+      ],
+      code: {
+        coding: item.code,
+      },
+      subject: {
+        reference: patientReference,
+      },
+      performer: [
+        {
+          reference: `Practitioner/N10000001`,
+          display: 'Voigt',
+        },
+      ],
+      encounter: {
+        reference: encounterReference,
+        display: encounterDisplay,
+      },
+      effectiveDateTime: state.visit.visit_date,
+      ...result,
+    };
+  }
+
+  /**
+   * Filters and maps observations based on the state and available lookup data.
+   */
+  function mapObservations(state) {
+    return state.visit.observations
+      .map(item => {
+        const { value_type: valueType, value } = item;
+        const dataObjects = [];
+
+        if (valueType === 'valueCodeableConcept') {
+          value.forEach(value => {
+            const result = {
+              [valueType]: {
+                coding: [JSON.parse(value.snomed_code)],
+                value: JSON.parse(value.value),
+              },
+            };
+            const data = createObservationData(
+              { ...item, id: value.id },
+              state,
+              result
+            );
+            dataObjects.push(data);
+          });
+        } else if (valueType === 'valueQuantity') {
+          const result = {
+            [valueType]: {
+              coding: [JSON.parse(value.snomed_code)],
+              value: parseFloat(value.value),
+            },
+          };
+          const data = createObservationData(
+            { ...item, id: value.id },
+            state,
+            result
+          );
+          dataObjects.push(data);
+        } else if (valueType === 'valueInteger') {
+          const result = {
+            [valueType]: {
+              coding: [JSON.parse(value.snomed_code)],
+              value: Number(JSON.parse(value.value)),
+            },
+          };
+          const data = createObservationData(
+            { ...item, id: value.id },
+            state,
+            result
+          );
+          dataObjects.push(data);
+        } else if (valueType === 'valueDateTime') {
+          const result = {
+            [valueType]: {
+              coding: [JSON.parse(value.snomed_code)],
+              value: new Date(JSON.parse(value.value)).toISOString(),
+            },
+          };
+          const data = createObservationData(
+            { ...item, id: value.id },
+            state,
+            result
+          );
+          dataObjects.push(data);
+        } else if (valueType === 'valueBoolean') {
+          const result = {
+            [valueType]: {
+              coding: [JSON.parse(value.snomed_code)],
+              value: JSON.parse(value.value) === 'never' ? false : true,
+            },
+          };
+          const data = createObservationData(
+            { ...item, id: value.id },
+            state,
+            result
+          );
+          dataObjects.push(data);
+        } else {
+          const result = {
+            [valueType]: {
+              coding: [JSON.parse(value.snomed_code)],
+              value: JSON.parse(value.value),
+            },
+          };
+
+          if (result) {
+            const data = createObservationData(item, state, result);
+            dataObjects.push(data);
+          }
+        }
+
+        return dataObjects;
+      })
+      .filter(data => data !== null);
+  }
+
+  state.observationMappedData = mapObservations(state);
+
+  return state;
+});
+
+post('Observation', $.observationMappedData);
+
+fn(state => {
+  state.observatioinData = state.data;
+  return state;
+});
