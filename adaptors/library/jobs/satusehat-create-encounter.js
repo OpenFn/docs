65a03a834270e01904dfbe57428fd5b6c2ac6dd0
Hunter Achieng <achienghunter@gmail.com>
2025-04-10 15:38:48 +0300

    fix: format with prettier
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- adaptors/library/jobs/satusehat-create-encounter.js
+++ adaptors/library/jobs/satusehat-create-encounter.js
@@ -5,93 +5,90 @@ fn(state => {
   state.encounterId = util.uuid();
 
   state.encounterMapping = {
+    id: state.encounterId,
+    resourceType: 'Encounter',
+    identifier: [
+      {
+        system: `http://sys-ids.kemkes.go.id/encounter/${state.organizationId}`,
+        value: state.visit.case_id,
+      },
+    ],
+    status: 'arrived',
+    class: {
+      system: 'http://terminology.hl7.org/CodeSystem/v3-ActCode',
+      code: 'AMB',
+      display: 'ambulatory',
+    },
+    subject: {
+      reference: 'Patient/P00805884304',
 
-
-      id: state.encounterId,
-      resourceType: 'Encounter',
-      identifier: [
-        {
-          system: `http://sys-ids.kemkes.go.id/encounter/${state.organizationId}`,
-          value: state.visit.case_id,
+      display: 'Elizabeth Dior',
+    },
+    participant: [
+      {
+        type: [
+          {
+            coding: [
+              {
+                system:
+                  'http://terminology.hl7.org/CodeSystem/v3-ParticipationType',
+                code: 'ATND',
+                display: 'attender',
+              },
+            ],
+          },
+        ],
+        individual: {
+          reference: `Practitioner/N10000001`,
+          display: 'Voigt',
         },
-      ],
-      status: 'arrived',
-      class: {
-        system: 'http://terminology.hl7.org/CodeSystem/v3-ActCode',
-        code: 'AMB',
-        display: 'ambulatory',
       },
-      subject: {
-        reference: 'Patient/P00805884304',
+    ],
+    period: {
+      start: state.visit.visit_date,
+      end: state.visit.visit_date,
+    },
 
-        display: 'Elizabeth Dior',
-      },
-      participant: [
-        {
-          type: [
-            {
-              coding: [
-                {
-                  system:
-                    'http://terminology.hl7.org/CodeSystem/v3-ParticipationType',
-                  code: 'ATND',
-                  display: 'attender',
+    location: [
+      {
+        location: {
+          reference: `Location/574d5ffd-2dc2-453b-a787-d1c63cc89ae4`,
+          display: `PLKB`,
+        },
+        extension: [
+          {
+            url: 'https://fhir.kemkes.go.id/r4/StructureDefinition/ServiceClass',
+            extension: [
+              {
+                url: 'value',
+                valueCodeableConcept: {
+                  coding: [
+                    {
+                      system:
+                        'http://terminology.kemkes.go.id/CodeSystem/locationServiceClass-Outpatient',
+                      code: 'reguler',
+                      display: 'Kelas Reguler',
+                    },
+                  ],
                 },
-              ],
-            },
-          ],
-          individual: {
-            reference: `Practitioner/N10000001`,
-            display: 'Voigt',
+              },
+            ],
           },
-        },
-      ],
-      period: {
-        start: state.visit.visit_date,
-        end: state.visit.visit_date,
+        ],
       },
-
-      location: [
-        {
-          location: {
-            reference: `Location/574d5ffd-2dc2-453b-a787-d1c63cc89ae4`,
-            display: `PLKB`,
-          },
-          extension: [
-            {
-              url: 'https://fhir.kemkes.go.id/r4/StructureDefinition/ServiceClass',
-              extension: [
-                {
-                  url: 'value',
-                  valueCodeableConcept: {
-                    coding: [
-                      {
-                        system:
-                          'http://terminology.kemkes.go.id/CodeSystem/locationServiceClass-Outpatient',
-                        code: 'reguler',
-                        display: 'Kelas Reguler',
-                      },
-                    ],
-                  },
-                },
-              ],
-            },
-          ],
+    ],
+    statusHistory: [
+      {
+        status: 'arrived',
+        period: {
+          start: state.visit.visit_date,
+          end: state.visit.visit_date,
         },
-      ],
-      statusHistory: [
-        {
-          status: 'arrived',
-          period: {
-            start: state.visit.visit_date,
-            end: state.visit.visit_date,
-          },
-        },
-      ],
-      serviceProvider: {
-        reference: `Organization/${state.organizationId}`,
       },
-    
+    ],
+    serviceProvider: {
+      reference: `Organization/${state.organizationId}`,
+    },
   };
 
   return state;
