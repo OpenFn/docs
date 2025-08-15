e594c20f1bb3f3c1ff932d2994db3f57a842b1f0
Hunter Achieng <achienghunter@gmail.com>
2025-08-15 16:47:35 +0300

    add pdf example
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- adaptors/library/jobs/generate-pdf.js
+++ adaptors/library/jobs/generate-pdf.js
@@ -16,8 +16,24 @@ fn(state => {
 
 generatePDF($.pdfHTMLContent, {
   sandbox: true,
-  filename:'trials.pdf', // Returns a JSON response containing an url parameter to PDFShift's Amazon S3 bucket.
+  filename: 'trials.pdf', // Returns a JSON response containing an url parameter to PDFShift's Amazon S3 bucket.
   // The URL expires after 48 hours
 });
 
+// Post generated PDF link to WhatsApp
+fn(state => {
+  const { data } = state;
+  const pdfData = JSON.parse(data);
+  return { ...state, pdfData };
+});
 
+request('POST', 'messages', state => ({
+  to: '254712345678',
+  body: `Please check on the PDF and download in 24hours. ${state.pdfData.url}`,
+  type: 'template',
+  template: {
+    name: 'hello_world',
+    language: { code: 'en_US' },
+  },
+  messaging_product: 'whatsapp',
+}));
