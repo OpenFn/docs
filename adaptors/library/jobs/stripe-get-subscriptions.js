8f2bf77896cdaf6034f316689191bbb874edf9fd
Hunter Achieng <achienghunter@gmail.com>
2025-08-29 22:32:21 +0300

    feat: add stripe docs
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

---
+++ adaptors/library/jobs/stripe-get-subscriptions.js
@@ -0,0 +1,14 @@
+// Get subscription data
+fn(state => {
+  state.subscriptions = state.data;
+  state.subscriptionsData = [];
+  return state;
+});
+
+each(
+  '$.subscriptions[*]',
+  get('subscriptions', $.data).then(state => {
+    state.subscriptionsData.push(state.data.data);
+    return state;
+  })
+);
