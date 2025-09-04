8c4c5386575c46d9a17bd312d71b90cae8f09fa6
Hunter Achieng <achienghunter@gmail.com>
2025-09-04 14:12:58 +0300

    feat: use md images for icons
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- generate-adaptors/index.js
+++ generate-adaptors/index.js
@@ -7,66 +7,19 @@ const versions = [];
 
 const badges = [
   {
-    name: 'core',
-    icon: ` <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1}
-      stroke="currentColor"
-      className=" "
-      width={20}
-      height={20}
-      aria-hidden="true"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
-      />
-    </svg>`,
-    emoji:'⛨',
+    name: 'Core',
+    icon: `/img/heroicons--shield-check.svg`,
+    link: '/adaptors#adaptor-badges',
   },
   {
-    name: 'community',
-    icon: `  <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
-      width={20}
-      height={20}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-      />
-    </svg>`,
-    emoji:'👥'
+    name: 'Community',
+    icon: ` /img/heroicons--users.svg`,
+    link: '/adaptors#adaptor-badges',
   },
   {
-    name: 'legacy',
-    icon: ` <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      fill="none" 
-      viewBox="0 0 24 24" 
-      strokeWidth={1.5} 
-      stroke="currentColor" 
-        
-      width={20}
-      height={20}  
-    >
-      <path 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
-        d="M4.5 12a7.5 7.5 0 0 0 15 0m-15 0a7.5 7.5 0 1 1 15 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077 1.41-.513m14.095-5.13 1.41-.513M5.106 17.785l1.15-.964m11.49-9.642 1.149-.964M7.501 19.795l.75-1.3m7.5-12.99.75-1.3m-6.063 16.658.26-1.477m2.605-14.772.26-1.477m0 17.726-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205 12 12m6.894 5.785-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495" 
-      />
-    </svg>
-`,
-emoji:'⚙️'
+    name: 'Legacy',
+    icon: ` /img/heroicons--cog.svg`,
+    link: '/adaptors#adaptor-badges',
   },
 ];
 async function listVersions(next) {
@@ -169,16 +122,23 @@ function generateJsDoc(a) {
 
   docsContent = parts.join('');
 
-  const icon = badges.find(b => b.name === a.badge?.toLowerCase())?.emoji || ''; 
+  const icon = badges.find(
+    b => b.name?.toLowerCase() === a.badge?.toLowerCase()
+  );
 
   return `---
-title: ${a.name}@${a.version}-${icon}
+title: ${a.name}@${a.version}
 id: ${a.name}-docs
 keywords:
   - adaptor
   - ${a.name}
   ${a.functions.length > 0 ? '- ' : ''}${a.functions.join('\r\n  - ')}
 ---
+
+
+#### Badge: [${icon?.name || ''} ${
+    icon?.icon ? `![badge](${icon.icon})` : ''
+  }](${icon?.link || '#'})
 
 ${docsContent}`;
 }
